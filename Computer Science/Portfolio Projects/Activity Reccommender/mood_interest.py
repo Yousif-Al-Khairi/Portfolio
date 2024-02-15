e35e39dd2cf16e45a776f42c584cc6136c06af2c
Yousif <95356845+Yousif-Al-Khairi@users.noreply.github.com>
2024-02-15 21:12:46 +0100

    added dictionary to help assist with collecting user input

---
+++ Computer Science/Portfolio Projects/Activity Reccommender/mood_interest.py
@@ -0,0 +1,18 @@
+mood_interest_choices = {
+    'a': 'Relaxing',
+    'b': 'Energetic',
+    'c': 'Social',
+    'd': 'Adventurous',
+    'e': 'Creative',
+    'f': 'Reflective',
+    'g': 'Productive',
+    'h': 'Spontaneous',
+    'i': 'Outdoor activities',
+    'j': 'Indoor activities',
+    'k': 'Social gatherings',
+    'l': 'Arts and crafts',
+    'm': 'Sports and fitness',
+    'n': 'Food and cooking',
+    'o': 'Cultural experiences',
+    'p': 'Educational pursuits'
+}
