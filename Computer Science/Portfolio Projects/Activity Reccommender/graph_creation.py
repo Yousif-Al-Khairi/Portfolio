a2316198c4755b6ee492168cf0903f1d93953854
Yousif <95356845+Yousif-Al-Khairi@users.noreply.github.com>
2024-02-18 21:12:11 +0100

    Added starting vertex, skeleton for search method in graph

--- Computer Science/Portfolio Projects/Activity Reccommender/graph_creation.py
+++ Computer Science/Portfolio Projects/Activity Reccommender/graph_creation.py
@@ -19,21 +19,37 @@ class Graph:
 
     def add_vertex(self,vertex): 
         self.graph_dict[vertex.value] = vertex
-
+    
     def add_edge(self, from_vertex, to_vertex, weight = 0):
         self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
         if not self.directed:
             self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+    def return_nodes(self):
+        return list(self.graph_dict.keys())
+    def search(self, interest_list):
+        checked = {}
+        RList = []
+        first_key = next(iter(self.graph_dict))  # Get the first key
+        start_vertex = self.graph_dict[first_key]
+        for mood_interest in start_vertex.get_edges():
+            if mood_interest in interest_list:
+                pass
+
+        return RList
+    
 
 
 #create the graph
 activity_graph = Graph(directed=True)  
-
+starting_vert = Vertex("Start")
+activity_graph.add_vertex(starting_vert)
 mood_interest_vertices = {}
+mood_interest_vertices["Start"] = starting_vert
 # populate the graph data structure using the AI generated activity_categories python dictionary
 for mood_interest in activity_categories.keys():
     vertex = Vertex(mood_interest)
     activity_graph.add_vertex(vertex)
+    activity_graph.add_edge(starting_vert, vertex)
     mood_interest_vertices[mood_interest] = vertex
 
 activity_vertices = {}
@@ -52,7 +68,10 @@ for mood_interest, activities in activity_categories.items():
         activity_graph.add_edge(mood_interest_vertex, activity_vertex)
 
 
-""" print("Mood/Interest Vertices:")
+
+print(activity_graph.return_nodes())
+""" 
+print("Mood/Interest Vertices:")
 for vertex in mood_interest_vertices.values():
     print(vertex.value, vertex.get_edges())
 
@@ -74,5 +93,8 @@ for vertex in activity_vertices.values():
 for vertex in mood_interest_vertices.values():
     for neighbor in vertex.get_edges():
         G.add_edge(vertex.value, neighbor)
-nx.draw(G, with_labels=True)
-plt.show() """
+pos = nx.spring_layout(G)
+
+nx.draw(G, with_labels=True, node_size=200, font_size=5)  # Adjust node size and font size as needed
+plt.show()
+#print(starting_vert.get_edges()) """
---
+++ Computer Science/Portfolio Projects/Activity Reccommender/main.py
@@ -0,0 +1,5 @@
+
+
+
+def welcome_func():
+    print("Welcome to the activity reccommender terminal application!!")
